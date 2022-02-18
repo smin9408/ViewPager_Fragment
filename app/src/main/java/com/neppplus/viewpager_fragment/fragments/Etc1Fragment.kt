a1a265c10fe524232240903be97719e678cc733d
smin9408 <stmdaks888@naver.com>
2022-02-18 17:12:19 +0900

    기타 프래그먼트 2개 추가  - 5장 짜리 ViewPager로 작업

---
+++ app/src/main/java/com/neppplus/viewpager_fragment/fragments/Etc1Fragment.kt
@@ -0,0 +1,27 @@
+package com.neppplus.viewpager_fragment.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpager_fragment.R
+
+class Etc1Fragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_etc1, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+
+    }
+
+}
