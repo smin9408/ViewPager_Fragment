72204ec9f46998e573b759a89451bc0f480316ad
smin9408 <stmdaks888@naver.com>
2022-02-21 09:55:12 +0900

    함수의 결과를 바로 지정

--- app/src/main/java/com/neppplus/viewpager_fragment/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpager_fragment/adapters/MainViewPagerAdapter.kt
@@ -6,9 +6,7 @@ import androidx.fragment.app.FragmentPagerAdapter
 import com.neppplus.viewpager_fragment.fragments.*
 
 class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
-        return 3
-    }
+    override fun getCount() = 3
 
     override fun getItem(position: Int): Fragment {
 
